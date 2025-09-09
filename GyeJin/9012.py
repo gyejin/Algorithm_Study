dedf46691a66e1040be059a0f03c72cc4160f0a7
pgj1394 <whrpwls1234@hanmail.net>
2025-09-09 01:42:23 +0000

    [week01-01] Baekjoon 9012 {자료구조, 문자열, 스택} + comment

--- GyeJin/9012.py
+++ GyeJin/9012.py
@@ -6,7 +6,7 @@ N = int(input())
 
 for i in range(N):
     cnt = 0
-    VPS = input()
+    VPS = input()       #항상.strip 기억하기
     if VPS[0] == ')':
         print("NO")
         continue
@@ -21,3 +21,32 @@ for i in range(N):
         print("YES")
     else:
         print("NO")
+
+"""
+#코드리뷰 정답
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for _ in range(N):
+    ps = input().strip() # .strip() 추가!
+    stack = []
+    is_vps = True
+
+    for char in ps:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            # 뺄 '('가 없는데 ')'가 나온 경우
+            if not stack:
+                is_vps = False
+                break
+            stack.pop()
+    
+    # 루프가 끝났는데 스택에 '('가 남아있는 경우
+    if stack:
+        is_vps = False
+        
+    print("YES" if is_vps else "NO")
+"""
